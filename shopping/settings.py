6f1ef530d6e6fe20d68bac7574178fc2a8e5f785
Aye Chan Han <boateng.ach27@gmail.com>
2020-08-31 11:34:57 +0630

    database exported with sql file

--- shopping/settings.py
+++ shopping/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'shopping.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'shopping',
+        'USER': 'myprojectuser',
+	    'PASSWORD': 'password',
+	    'HOST': 'localhost',
+	    'PORT': '',
     }
 }
+
 
 
 # Password validation
