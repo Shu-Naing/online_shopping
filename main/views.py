05f04fd8c4614f5bc73319ce08396ac5b1c6bb6a
shunainghtet <shunainghtet.dev@hotmail.com>
2020-09-01 17:34:37 +0630

    login with account

--- main/views.py
+++ main/views.py
@@ -1,38 +1,25 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect, loader, reverse
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
-from django.contrib.auth import login, authenticate
-from .forms import RegistrationForm
 from django.template import RequestContext
-from django.contrib import messages
 from .models import Customer
 
 @csrf_protect
 def registration(request):
     if request.method == "GET":
-        form = RegistrationForm()
+        return render(request, 'registration.html')
     elif request.method == "POST":
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            if not Customer.objects.filter(customer_email = form.cleaned_data['customer_email']).exists():
-                customer = Customer(
-                    customer_username = form.cleaned_data['customer_username'],
-                    customer_firstname = form.cleaned_data['customer_firstname'],
-                    customer_lastname = form.cleaned_data['customer_lastname'],
-                    customer_email = form.cleaned_data['customer_email'],
-                    customer_password = form.cleaned_data['customer_password'],
-                )
-                customer.save()
-                return redirect(login)
-            else:
-                messages.add_message(request, messages.WARNING, 'Email already taken!!')
-    return render(request, 'registration.html', {'form': form})
+        customer = Customer(
+            customer_name = request.POST['username'],
+            customer_email = request.POST['email'],
+            customer_password = request.POST['password'],
+        )
+        customer.save()
+        return redirect('/login')
 
 @csrf_protect
 def login(request):
     if request.method == "GET":
-        return render(request, 'login.html')
-    # elif request.method == "POST":
-    #     return redirect(reverse('login'))
+        return render(request, "login.html")
 
 # Create your views here.
