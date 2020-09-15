f95294e90dbd6140471991f53c5ecf28a850e376
Shu-Naing <shunainghtet.dev@hotmail.com>
2020-09-15 11:19:55 +0630

    search and code modification at frontend and backend

---
+++ static/static_dirs/js/nn.js
@@ -0,0 +1,8 @@
+$('.header-nav').on('click', '.search-toggle', function(e) {
+    var selector = $(this).data('selector');
+  
+    $(selector).toggleClass('show').find('.search-input').focus();
+    $(this).toggleClass('active');
+  
+    e.preventDefault();
+  });
