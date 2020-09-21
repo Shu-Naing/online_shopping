8911623a644f0f7c47ec25ec33bc7156d44b4522
Aye Chan Han <boateng.ach27@gmail.com>
2020-09-21 17:16:24 +0630

    single product add to cart now added new table row without refreshing web page

--- static/static_dirs/js/cart.js
+++ static/static_dirs/js/cart.js
@@ -22,6 +22,10 @@ $(function () {
         if ($(span).text() == "") {
           $(span).text(0);
         }
+        var sp_product = $("#sp_productName").text()
+        var sp_price = $("#sp_productPrice").text().slice(1)
+        var totalPrice = parseFloat(sp_price) * $(qty).val()
+        $("#cartTable").append('<tr><td>'+ sp_product +'</td><td>'+ $(qty).val() +'</td><td>'+ totalPrice +'</td><td><button id="delete" data-id="'+ sp_product +'" onclick = "removeProduct(this)" class="delete">Remove</button></td></tr>');
         no_of_cart = parseInt($(span).text(), 10) + 1;
         $(span).text(no_of_cart);
         alert("Successfully added to cart!");
